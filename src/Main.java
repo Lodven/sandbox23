287590aa9d6f6cf5727fce35fcfd33b6570f7b30
Lodven <sonic777222555@gmail.com>
2016-11-16 18:28:34 +0300

    Add method init()

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
 public class Main {
 
     public Main(){
-        calc = new Calculator(
-                new Summator(),
-                new Amplifier(),
-                new Contractor(),
-                new Diveder()
-        );
+    init();
+    }
 
-        System.out.println(calc.amp(
-                new FInteger(3),
-                new FInteger(4)
-        ).getValue());
-        System.out.println(calc.sum(
-                new FInteger(4),
-                new FInteger(12)
-        ).getValue());
-        System.out.println(calc.cot(
-                new FInteger(2),
-                new FInteger(5)
-        ).getValue());
-        System.out.println(calc.div(
-                new FInteger(8),
-                new FInteger(2)
-        ).getValue());
+    public void init(){
+            calc = new Calculator(
+                    new Summator(),
+                    new Amplifier(),
+                    new Contractor(),
+                    new Diveder()
+            );
+
+            System.out.println(calc.amp(
+                    new FInteger(3),
+                    new FInteger(4)
+            ).getValue());
+            System.out.println(calc.sum(
+                    new FInteger(4),
+                    new FInteger(12)
+            ).getValue());
+            System.out.println(calc.cot(
+                    new FInteger(2),
+                    new FInteger(5)
+            ).getValue());
+            System.out.println(calc.div(
+                    new FInteger(8),
+                    new FInteger(2)
+            ).getValue());
+
     }
 
     public static Calculator calc;
